cb73d5387e5016cffda45a0ffec50a79f16023c4
andreslucer <andres220799@gmail.com>
2022-10-13 15:43:27 -0300

    end commit

---
+++ src/components/weatherApp.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+import WeatherMainInfo from "./weatherMainInfo"
+import WeatherForm from "./wheaterForm"
+
+import styles from "../components/weatherApp.module.css"
+
+export default function WeatherApp(){
+    const [weather, setWeather] = useState(null)
+
+    useEffect(()=>{
+        loadInfo()
+    },[])
+
+    useEffect(()=>{
+        document.title = `Weather | ${weather?.location.name?? ""}`
+    },[weather])
+
+   async function loadInfo(city= "london"){
+    try {
+        const request = await fetch(`${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_KEY}&q=${city}`);
+
+        const json= await request.json();
+
+        setWeather(json)
+    } catch (error) {
+        
+    }
+    }
+
+    function handleChangeCity(city){
+        setWeather(null); 
+        loadInfo(city)
+    }
+    return <div className={styles.wheaterContainer}>
+        <WeatherForm onChangeCity={handleChangeCity}/>
+        <div><WeatherMainInfo weather={weather}/></div>
+    </div>
+}
